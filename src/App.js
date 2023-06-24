ffb87aaa4ae73099608829cf139efbabf68369a0
SatyamMattoo <satyammattoo2003gmail.com>
2023-06-25 00:04:22 +0530

    initialising first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/app.css";
+import {
+  signOut,
+  onAuthStateChanged,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import  app  from "./firebase.js";
+import { useEffect, useState } from "react";
+
+const auth = getAuth(app);
+const logOut = () => signOut(auth);
+
+const loginGoogle = () => {
+  const providerGoogle = new GoogleAuthProvider();
+  signInWithPopup(auth, providerGoogle);
+};
 
 function App() {
+
+  const [user,setUser]= useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth,(data)=>{
+    setUser(data);
+    });
+    return () => {
+      unsubscribe();
+    }
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+    (!user)?
+      <div className="box">
+        <div className="left-side">
+          <div className="board">
+            <p>Board.</p>
+          </div>
+        </div>
+        <div className="right-side">
+          <div className="signin">
+            <h1>Sign In</h1>
+            <p>Sign In to your account</p>
+            <div className="options">
+              <button className="google" onClick={loginGoogle}>
+                <img
+                  src="https://img.icons8.com/?size=512&id=V5cGWnc9R4xj&format=png"
+                  alt=""
+                  height={"15px"}
+                />
+                <p>Sign In with Google</p>
+              </button>
+              <button className="apple">
+                <img
+                  src="https://img.icons8.com/?size=512&id=890&format=png"
+                  alt=""
+                  height={"15px"}
+                />
+                <p>Sign In with Apple</p>
+              </button>
+            </div>
+            <form className="form">
+              <p>Email address</p>
+              <input type="text" placeholder="Enter your email" />
+              <p>Password</p>
+              <input type="password" placeholder="Enter your password" />
+              <a href="/">Forgot Password?</a>
+              <button className="submitBtn">Sign In</button>
+            </form>
+            <p className="register">
+              Don't have a account? <a href="/">Register here</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    :
+    <div className="container">
+      <button onClick={logOut}>logout</button>
+    </div>  
+    }
+    </>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4a1iLbIIzZCN3776Si-y00iixh9ZYH9Y",
+  authDomain: "listed-assignment-satyam.firebaseapp.com",
+  projectId: "listed-assignment-satyam",
+  storageBucket: "listed-assignment-satyam.appspot.com",
+  messagingSenderId: "56946772788",
+  appId: "1:56946772788:web:85c5b5241f167267c5b361"
+};
+
+// Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ export default app;
